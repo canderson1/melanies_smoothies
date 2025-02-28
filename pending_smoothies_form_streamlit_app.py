2771f82465025b28a7abbc55a9a5b1fdf35b45c7
Charlotte A <clanderson86@gmail.com>
2025-02-28 15:48:41 +0000

    Add files via upload

---
+++ pending_smoothies_form_streamlit_app.py
@@ -0,0 +1,38 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+# Write directly to the app
+st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
+st.write(
+    f"""Orders to fill!
+    """
+)
+
+# Get the current credentials
+session = get_active_session()
+
+orders_df = (
+    session.table("smoothies.public.orders")
+    .filter(col('ORDER_FILLED') == False)  # Filter where ORDER_FILLED is False
+)
+
+if orders_df.count() > 0:
+    # st.dataframe(data=orders_df, use_container_width=True)
+    editable_df = st.data_editor(orders_df)
+    
+    time_to_insert = st.button('Submit')
+    if time_to_insert:
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+        try:
+            og_dataset.merge(edited_dataset
+                , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
+                , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+            )
+            st.success('Order filled!', icon="👍")
+        except:
+            st.write('Error: Something went wrong submit again')
+else:
+    st.write('No pending orders')
